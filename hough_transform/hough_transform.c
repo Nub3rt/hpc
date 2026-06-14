53e7bb3e9f0984c1172cc751ef8506c9c3f4e6ec
Norbert Varga <norbert.varga@studenti.unitn.it>
2026-06-14 21:44:16 +0200

    Hough transform cleaned up and in pure C, stb_image.h

--- hough_transform/hough_transform.c
+++ hough_transform/hough_transform.c
@@ -1,73 +1,50 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 
-#include <iostream>
-
-#include <opencv2/opencv.hpp>
-
-const int GAUSSIAN_KERNEL_SIZE = 5;
-const double GAUSSIAN_SIGMA = 2.0;
-const double CANNY_LOW_THRESHOLD = 150.0;
-const double CANNY_HIGH_THRESHOLD = 200.0;
+#define STB_IMAGE_IMPLEMENTATION
+#include "libs/stb_image.h"
 
 const double DEG2RAD = 3.1419 / 180.0;
 
-int main( ) {
-  cv::Mat img = cv::imread( "in.jpg", cv::IMREAD_COLOR_BGR );
+int main( int argc, char** argv ) {
+  if ( argc < 3 ) {
+    printf( "usage: hough_transform.out <img_path> <theta_multiplier> [<print>]\n" );
+    return 1;
+  }
 
-  cv::Mat gray;
-  cv::cvtColor( img, gray, cv::COLOR_BGR2GRAY );
-
-  cv::Mat edges( gray );
-  cv::GaussianBlur(
-      edges,
-      edges,
-      cv::Size( GAUSSIAN_KERNEL_SIZE, GAUSSIAN_KERNEL_SIZE ),
-      GAUSSIAN_SIGMA
-  );
-  cv::Canny( edges, edges, CANNY_LOW_THRESHOLD, CANNY_HIGH_THRESHOLD );
-
-  cv::imwrite( "canny.jpg", edges );
-
-
-  if (!edges.isContinuous())
-    edges = edges.clone();
-  unsigned char* data = edges.data;
-  int w = edges.cols;
-  int h = edges.rows;
-
-  // maximum distance in image from origo (origo in middle of image)
-  double max_distance = sqrt( 2.0 ) * (h > w ? h : w) / 2.0;
-  // r range
-  int acc_h = max_distance * 2.0;
-  // theta range
-  int acc_w = 180;
-
-  // accumulator
-  unsigned int* acc = (unsigned int*)calloc( acc_h * acc_w, sizeof( unsigned int ) );
+  int w, h;
+  unsigned char* img = stbi_load( argv[ 1 ], &w, &h, NULL, 1 );
 
   int center_x = w / 2;
   int center_y = h / 2;
 
+  // maximum distance in image from origo (origo in middle of image)
+  double max_distance = sqrt( 2.0 ) * (h > w ? h : w) / 2.0;
+  // rho range
+  int acc_h = max_distance * 2.0;
+  // theta range
+  int t_mult = atoi( argv[ 2 ] );
+  int acc_w = 180 * t_mult;
+
+  // accumulator
+  int* acc = calloc( acc_h * acc_w, sizeof( int ) );
+
   // Voting phase
-  int n_edges = 0;
   for ( int y = 0; y < h; ++y ) {
     for ( int x = 0; x < w; ++x ) {
-      if ( data[ y * w + x ] > 250 ) {
-        ++n_edges;
-        for ( int t = 0; t < 180; ++t ) {
-          double r = (x - center_x) * cos( t * DEG2RAD ) + (y - center_y) * sin( t * DEG2RAD );
-          acc[ ((int)(r + max_distance)) * 180 + t ] += 1;
+      if ( img[ y * w + x ] > 250 ) {
+        for ( int t = 0; t < acc_w; ++t ) {
+          double t_deg = t / (double)t_mult;
+          double r = (x - center_x) * cos( t_deg * DEG2RAD ) + (y - center_y) * sin( t_deg * DEG2RAD );
+          acc[ ((int)(r + max_distance)) * acc_w + t ] += 1;
         }
       }
     }
   }
 
-  std::cout << "Edge count: " << n_edges << std::endl;
-
 
   // Find maximum accumulator value
-  unsigned int max_acc = 0;
+  int max_acc = 0;
   for ( int r = 0; r < acc_h; ++r ) {
       for ( int t = 0; t < acc_w; ++t ) {
           if ( acc[ r * acc_w + t ] > max_acc )
@@ -75,28 +52,12 @@ int main( ) {
       }
   }
 
-  std::cout << "max_acc: " << max_acc << std::endl;
-
-  // Create image (rows = r, cols = theta)
-  cv::Mat acc_img( acc_h, acc_w, CV_8UC1 );
-
-  for ( int r = 0; r < acc_h; ++r ) {
-    for ( int t = 0; t < acc_w; ++t ) {
-        unsigned int value = acc[ r * acc_w + t ];
-        acc_img.at<uchar>(r, t) = static_cast<uchar>(255.0 * value / max_acc);
-      }
-  }
-
-  cv::imwrite("accumulator.jpg", acc_img);
-
-
   // Select local maxima
-  int threshold = h > w ? h : w;
-  threshold /= 4;
-  threshold = max_acc * 0.8;
+  int threshold = max_acc * 0.8;
 
   int max_count = 50;
-  int* lines = (int*)malloc( max_count * 2 * sizeof( int ) );
+  int* rs = malloc( max_count * sizeof( int ) );
+  double* ts = malloc( max_count * sizeof( double ) );
   int count = 0;
 
 
@@ -122,61 +83,23 @@ int main( ) {
           continue;
 
         if ( count < max_count ) {
-          lines[ count * 2 ] = r;
-          lines[ count * 2 + 1 ] = t;
+          rs[ count ] = r - max_distance;
+          ts[ count ] = t / (double)t_mult;
           ++count;
         }
       }
     }
   }
 
-  std::cout << count << " lines:\n";
-//  for ( int i = 0; i < count; ++i )
-//    std::cout << "r = " << lines[ i * 2 ]
-//              << ", theta = " << lines[ i * 2 + 1 ] << std::endl;
+  printf( "%i lines:\n", count );
+  if ( argc > 3 && atoi( argv[ 3 ] ) )
+    for ( int i = 0; i < count; ++i )
+      printf( "rho = %5i, theta = %6.2f\n", rs[ i ], ts[ i ] );
 
-  cv::Mat result = img.clone();
-
-  for ( int i = 0; i < count; ++i ) {
-    int r_idx = lines [i * 2 ];
-    int theta_deg = lines[ i * 2 + 1 ];
-
-    double r = r_idx - max_distance;
-    double theta = theta_deg * DEG2RAD;
-
-    double a = cos( theta );
-    double b = sin( theta );
-
-    // Point on the line
-    double x0 = center_x + a * r;
-    double y0 = center_y + b * r;
-
-    // Two far-away points defining the line
-    cv::Point p1(
-        cvRound( x0 + 10000 * (-b) ),
-        cvRound( y0 + 10000 * ( a) )
-    );
-
-    cv::Point p2(
-        cvRound( x0 - 10000 * (-b) ),
-        cvRound( y0 - 10000 * ( a) )
-    );
-
-    cv::line(
-        result,
-        p1,
-        p2,
-        cv::Scalar( 0, 0, 255 ), // red
-        5,
-        cv::LINE_AA
-    );
-  }
-
-  cv::imwrite("out.jpg", result);
-
-
+  stbi_image_free( img );
   free( acc );
-  free( lines );
+  free( rs );
+  free( ts );
 
   return 0;
 }
